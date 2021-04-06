fdc4e47c6354f2a59bfb03069098c798cd125548
Amaal Jama <amaaljama111@gmail.com>
2021-04-06 17:51:53 +0100

    message

---
+++ src/components/Cards.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Carditem from './Carditem';
+import './Cards.css';
+
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC destinations!</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <Carditem 
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterall deepinside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        <Carditem
+                        src='images/img-3.jpg'
+                        text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                        label='Mystery'
+                        path='/services'
+                        />
+                        <Carditem
+                        src='images/img-4.jpg'
+                        text='Experience Football on Top of the Himilayan Mountains'
+                        label='Adventure'
+                        path='/products'
+                        />
+                    </ul>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Cards
